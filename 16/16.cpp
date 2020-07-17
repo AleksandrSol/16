e93860cbb389c2eaee926b2569c23a1cd5fff7c0
AleksandrSol <66996293+AleksandrSol@users.noreply.github.com>
2020-07-17 17:28:45 +0300

    Добавьте файлы проекта.

---
+++ 16/16.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    cout << "++++++++++++++++++++++++++++++++++++\n";
+    cout << "            Iнформацiя:           \n";
+    cout << "Група: IПЗ-I 18к                  \n";
+    cout << "ПІБ: Соляной Александр Олегович\n";
+    cout << "Номер теми: 1.1 \n";
+    cout << "Номер завдання: 16 " << "\n";
+    cout << "+++++++++++++++++++++++++++++++++++\n\n";
+
+    int mas[10] = { 1,10,2,20,3,30,4,40,5,50 };
+    for (int i = 0; i < 10; i += 2)
+    {
+        cout << mas[i] << '\t' << mas[i + 1] << '\n';
+
+    }
+    system("pause>>void");
+}
